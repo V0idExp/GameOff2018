5d1addb8054a6da61fe2bd89ef1c88fe1e2b383f
Ivan Nikolaev <voidexp@gmail.com>
2018-11-04 00:03:07 +0300

    Game over (reload scene on entering water)

---
+++ Assets/GameOverController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour {
+
+    bool alreadyTriggered = false;
+
+	public void StartGameOver()
+    {
+        if (alreadyTriggered)
+        {
+            return;
+        }
+
+        var audio = GetComponent<AudioSource>();
+        var sleep = audio.clip.length + 1.0f;
+        audio.Play();
+        StartCoroutine(ReloadAfter(sleep));
+
+        alreadyTriggered = true;
+    }
+
+    IEnumerator ReloadAfter(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        alreadyTriggered = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/GameOverTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            TriggerGameOver();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            TriggerGameOver();
+        }
+    }
+
+    private void TriggerGameOver()
+    {
+        GameObject.Find("GameOverController").GetComponent<GameOverController>().StartGameOver();
+    }
+}
