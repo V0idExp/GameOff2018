bbc08ddbfdfc25a180f6d4fd41e15b82c097d557
Ivan Nikolaev <voidexp@gmail.com>
2018-11-17 20:59:59 +0300

    Coffee stick lever trap

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,28 +3,15 @@
 public class PlayerController : MonoBehaviour {
 
     public float maxSpeed = 2f;
-    public float jumpForce = 500f;
-    public bool jumpEnabled = false;
 
-    public Transform probe;
-    public LayerMask groundMask;
-
-    public AudioClip jumpSound;
-
-    Animator animator;
     Rigidbody2D body;
     SpriteRenderer sprite;
-    new AudioSource audio;
-
-    bool grounded = false;
 
     // Use this for initialization
     void Start()
     {
-        animator = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-        audio = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -37,29 +24,10 @@ public class PlayerController : MonoBehaviour {
         float speed = Input.GetAxis("Horizontal") * maxSpeed;
         body.velocity = new Vector2(speed, body.velocity.y);
 
-        if (jumpEnabled)
-        {
-            grounded = Physics2D.OverlapCircle(probe.position, 0.1f, groundMask);
-            if (grounded && Input.GetButton("Jump"))
-            {
-                Jump();
-            }
-        }
-
         bool flip = sprite.flipX ? (speed > 0) : (speed < 0);
         if (flip)
         {
             sprite.flipX = !sprite.flipX;
         }
-
-        animator.SetFloat("speed", Mathf.Abs(speed));
-        animator.SetFloat("vspeed", Mathf.Abs(body.velocity.y));
-    }
-
-    private void Jump()
-    {
-        grounded = false;
-        body.AddForce(new Vector2(0f, jumpForce));
-        audio.PlayOneShot(jumpSound);
     }
 }
