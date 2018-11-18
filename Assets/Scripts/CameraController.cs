0d876a9736bf89c6d579bcf25ef61a11df9e8192
A. Shibaylo <shibailoav@gmail.com>
2018-11-18 11:10:39 +0300

    Added camera following player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject playerObject;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - playerObject.transform.position;
+	}
+	
+	void LateUpdate () {
+        transform.position = playerObject.transform.position + offset;
+	}
+}
